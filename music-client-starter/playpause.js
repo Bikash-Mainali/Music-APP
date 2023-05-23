5b0389922c6a521e21b969298863b2dd791acb80
Biki <b.mainali02@gmail.com>
2023-05-22 20:48:53 -0500

    player section managed

---
+++ music-client-starter/playpause.js
@@ -0,0 +1,148 @@
+// let now_playing = document.querySelector(".now-playing");
+// let song_art = document.querySelector(".song-art");
+// let song_name = document.querySelector(".song-name");
+// let song_artist = document.querySelector(".song-artist");
+
+// let playpause_btn = document.querySelector(".playpause-song");
+// let next_btn = document.querySelector(".next-song");
+// let prev_btn = document.querySelector(".prev-song");
+
+// let seek_slider = document.querySelector(".seek_slider");
+// let volume_slider = document.querySelector(".volume_slider");
+// let curr_time = document.querySelector(".current-time");
+// let total_duration = document.querySelector(".total-duration");
+
+// let song_index = 0;
+// let isPlaying = false;
+// let updateTimer;
+
+// // Create new audio element
+// let current_song = document.createElement('audio');
+
+// // Define the songs that have to be played
+// let song_list = [
+//   {
+//     name: "Night Owl",
+//     artist: "Broke For Free",
+//     image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+//     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
+//   },
+//   {
+//     name: "Enthusiast",
+//     artist: "Tours",
+//     image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+//     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
+//   },
+//   {
+//     name: "Shipping Lanes",
+//     artist: "Chad Crouch",
+//     image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+//     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
+//   },
+// ];
+
+// function random_bg_color() {
+
+// //   // Get a number between 64 to 256 (for getting lighter colors)
+// //   let red = Math.floor(Math.random() * 256) + 64;
+// //   let green = Math.floor(Math.random() * 256) + 64;
+// //   let blue = Math.floor(Math.random() * 256) + 64;
+
+// //   // Construct a color withe the given values
+// //   let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
+
+// //   // Set the background to that color
+// //   document.body.style.background = bgColor;
+// }
+
+// function loadSong(song_index) {
+//   clearInterval(updateTimer);
+//   resetValues();
+//   current_song.src = song_list[song_index].path;
+//   current_song.load();
+
+//   song_art.style.backgroundImage = "url(" + song_list[song_index].image + ")";
+//   song_name.textContent = song_list[song_index].name;
+//   song_artist.textContent = song_list[song_index].artist;
+//   now_playing.textContent = "PLAYING " + (song_index + 1) + " OF " + song_list.length;
+
+//   updateTimer = setInterval(seekUpdate, 1000);
+//   current_song.addEventListener('ended', nextSong);
+//   random_bg_color();
+// }
+
+// function resetValues() {
+//   curr_time.textContent = "00:00";
+//   total_duration.textContent = "00:00";
+//   seek_slider.value = 0;
+// }
+
+// // Load the first song in the songlist
+// loadSong(song_index);
+
+// function playpauseSong() {
+//   if (!isPlaying) playSong();
+//   else pauseSong();
+// }
+
+// function playSong() {
+//   current_song.play();
+//   isPlaying = true;
+//   playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+// }
+
+// function pauseSong() {
+//   current_song.pause();
+//   isPlaying = false;
+//   playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+// }
+
+// function nextSong() {
+//   if (song_index < song_list.length - 1)
+//     song_index += 1;
+//   else song_index = 0;
+//   loadSong(song_index);
+//   playSong();
+// }
+
+// function prevSong() {
+//   if (song_index > 0)
+//     song_index -= 1;
+//   else song_index = song_list.length;
+//   loadSong(song_index);
+//   playSong();
+// }
+
+// function seekTo() {
+//   let seekto = current_song.duration * (seek_slider.value / 100);
+//   current_song.currentTime = seekto;
+// }
+
+// function setVolume() {
+//   current_song.volume = volume_slider.value / 100;
+// }
+
+// function seekUpdate() {
+//   let seekPosition = 0;
+
+//   if (!isNaN(current_song.duration)) {
+//     seekPosition = current_song.currentTime * (100 / current_song.duration);
+
+//     seek_slider.value = seekPosition;
+
+//     let currentMinutes = Math.floor(current_song.currentTime / 60);
+//     let currentSeconds = Math.floor(current_song.currentTime - currentMinutes * 60);
+//     let durationMinutes = Math.floor(current_song.duration / 60);
+//     let durationSeconds = Math.floor(current_song.duration - durationMinutes * 60);
+
+//     if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+//     if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+//     if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+//     if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+//     curr_time.textContent = currentMinutes + ":" + currentSeconds;
+//     total_duration.textContent = durationMinutes + ":" + durationSeconds;
+//   }
+// }
+
+
--- music-client-starter/welcome.js
+++ music-client-starter/welcome.js
@@ -1,16 +1,43 @@
 
-const BASE_URL = 'http://localhost:3000/api/';
-const TOKEN = localStorage.getItem('token');
+const BASE_URL = 'http://localhost:3000';
+// const TOKEN = localStorage.getItem('token');
 const USERNAME = localStorage.getItem('username');
-const HEADERS = {
-    'Authorization': `Bearer ${TOKEN}`,
-    'Content-Type': 'application/json'
-}
+
+let nowPlaying = document.querySelector(".now-playing");
+let songTitle = document.querySelector(".song-title");
+let playpauseBtn = document.querySelector(".playpause-song");
+let nextBtn = document.querySelector(".next-song");
+let prevBtn = document.querySelector(".prev-song");
+let playMode = document.querySelector(".play-mode");
+let seekSlider = document.querySelector(".seek-slider");
+let volumeSlider = document.querySelector(".volume-slider");
+let currTime = document.querySelector(".current-time");
+let totalDuration = document.querySelector(".total-duration");
+
+
+let currentSongIndex = 0;
+let isPlaying = false;
+let suffleMode = false;
+let repeatMode = false;
+let defaultMode = true;
+let setModeCount = 0;
+
+let playModeMap = new Map();
+playModeMap.set('defualt', 0);
+playModeMap.set('repeat', 1);
+playModeMap.set('suffle', 2);
+
+let updateTimer;
+let playListBackUp;
+
+// Create new audio element
+let currentSong = document.createElement('audio');
 
 window.onload = function () {
     //apply http interceptor
     if (!TOKEN) {
-        location.href = 'login.html';
+        //location.href = 'login.html';
+        document.getElementById('music-section').style.display = 'none';
     }
     init();
     document.getElementById('logoutBtn').onclick = logout;
@@ -26,39 +53,40 @@ function init() {
 function logout() {
     localStorage.removeItem('username');
     localStorage.removeItem('token');
-    location.href = 'login.html';
+    //can be made single plage with css
+    //location.href = 'login.html';
+    
 }
 
 async function getAllSongs() {
-    const response = await fetch(BASE_URL + '/music', {
+    const response = await fetch(BASE_URL + '/api/music', {
         headers: HEADERS
     })
     const songs = await response.json();
     populateToMusicTable(songs);
 }
 async function getAllPlayListSongs() {
-    const response = await fetch(BASE_URL + '/playlist', {
+    const response = await fetch(BASE_URL + '/api/playlist', {
         headers: HEADERS
     })
     const playList = await response.json();
+    playListBackUp = playList;
     populateToPlayListTable(playList);
-    if(isPlayListEmpty){
-        debugger
+    if (isPlayListEmpty(playList)) {
         document.getElementById("empty-table").innerHTML = `<small>Songs not added yet</small>`;
-        document.getElementById('song-play-section').innerHTML = ''
-    }else{
+        document.querySelector('.player').style.display = 'none';
+    } else {
         document.getElementById("empty-table").innerHTML = `<small>${playList.length} songs in your play list</small>`;
     }
-
 }
 
 populateToMusicTable = (songs) => {
     let tBody = ''
     songs.forEach((song, index) => {
         tBody += `
-        <tr id="row${index}">
+        <tr id="row${index + 1}">
         <td>
-            <span>${index}</span>
+            <span>${index + 1}</span>
             <span style="display:none">${song.id}</span>
         </td>
         <td>${song.title}</td>
@@ -92,11 +120,15 @@ populateToMusicTable = (songs) => {
 populateToPlayListTable = (playList) => {
     let tBody = ''
     playList.forEach((song, index) => {
-        tBody += `<tr id="row${index}"}>
-        <td>${song.songId}</td>
+        tBody += `
+        <tr id="row${index + 1}">
+        <td>
+            <span>${index + 1}</span>
+            <span style="display:none">${song.songId}</span>
+        </td>
         <td>${song.title}</td>
         <td><button class="fa fa-minus-circle add-to-play-list" aria-hidden="true" value=${song.songId} onclick="removeFromPlayList(this)"></button></td>
-        <td><button class="fa fa-play"  value=${song.urlPath} onclick="playSong(this)"></button></td>
+        <td><button value='${index}' onclick="playpauseSong(this)"><i class="fa fa-play-circle fa-2x"></button></td>
         </tr>`
     });
     let playListTable = `<h2>Your Playlist</h2>
@@ -114,17 +146,49 @@ populateToPlayListTable = (playList) => {
 
 }
 
-function playSong(currentSong){
-    const urlPath = currentSong.value;
-    document.getElementById('song-play-section').innerHTML = 
-    `<audio controls>
-        <source id="audio-source" src="http://localhost:3000/${urlPath}" type="audio/mp3">
-    </audio>`
-}
+
+// function playSong(currentBtn){
+//     debugger
+//     const urlPath = JSON.parse(currentBtn.value).urlPath;
+//     const runningSongId = JSON.parse(currentBtn.value).songId;
+//     const title = JSON.parse(currentBtn.value).title;
+//     let audioContent = `
+//                     <p id="playing-song">${title}</p>
+//                     <audio id="${runningSongId}" controls>
+//                         <source id="audio-source" 
+//                                 src="${BASE_URL +'/'}${urlPath}" 
+//                                 type="audio/mp3">
+//                         </source>
+//                     </audio>
+//                     <button id="prev-btn">Prev</button>
+//                     <button id="next-btn">Next</button>
+
+//                     `
+
+//     document.getElementById('song-play-section').innerHTML = audioContent;
+
+//     currentBtn.className = 'fa fa-pause';
+//     currentBtn.value = currentBtn.value;
+//     document.getElementById(runningSongId).play();
+//     currentBtn.setAttribute('onclick', `pauseSong(this)`);
+// }
+
+// function pauseSong(currentBtn){
+//     currentBtn.className = 'fa fa-play';
+//     currentBtn.value = currentBtn.value;
+//     document.getElementById(JSON.parse(currentBtn.value).songId).pause();
+//     currentBtn.setAttribute('onclick', `playSong(this)`);
+// }
+
+//to do
+//set all other songs in stopped mode
+// function setOthersIdle(){
+
+// }
 
 async function addToPlayList(currentRow) {
     console.log("adding song to the playlist");
-    const response = await fetch(`${BASE_URL}/playlist/add`, {
+    const response = await fetch(`${BASE_URL}/api/playlist/add`, {
         method: 'POST',
         body: JSON.stringify({
             "songId": currentRow.value
@@ -132,32 +196,193 @@ async function addToPlayList(currentRow) {
         headers: HEADERS
     });
     //refresh play list
-    if(response.status){
-    //document.getElementById("myBtn").disabled = true;
-    getAllPlayListSongs();
+    if (response.status) {
+        getAllPlayListSongs();
     }
 }
 
 async function removeFromPlayList(currentRow) {
-    // const rowId = current.parentNode.parentNode.id;
-    // const targetRow = document.getElementById(rowId);
-    // const songId = targetRow.getElementsByTagName('td')[0].innerHTML;
-
     console.log("removing song from the playlist");
-    const response = await fetch(`${BASE_URL}/playlist/remove`, {
+    const response = await fetch(`${BASE_URL}/api/playlist/remove`, {
         method: 'POST',
         body: JSON.stringify({
             "songId": currentRow.value
         }),
         headers: HEADERS
     });
+    if (currentSong.value === currentRow.value) {
+        //clear song from current player section
+        resetPlayerSection();
+    }
     //refresh play list
+
     getAllPlayListSongs();
+}
+function resetPlayerSection() {
+    currentSong.src = '';
+    songTitle.textContent = ''
+    nowPlaying.textContent = ''
+    playpauseBtn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+    clearInterval(updateTimer);
+    resetValues();
 }
 
 function isPlayListEmpty(playList) {
-return playList.length != 0;
+    return playList.length == 0;
 }
+
+function loadSong(songIndex) {
+    currentSongIndex = parseInt(songIndex);
+    document.querySelector(".player").style.display = 'block';
+    clearInterval(updateTimer);
+    resetValues();
+    currentSong.src = BASE_URL + "/" + playListBackUp[currentSongIndex].urlPath;
+    currentSong.value = playListBackUp[currentSongIndex].songId;
+    //currentSong.load();
+    nowPlaying.textContent = `PLAYING ${parseInt(currentSongIndex) + 1}  OF ${playListBackUp.length}`;
+    songTitle.textContent = playListBackUp[currentSongIndex].title;
+    updateTimer = setInterval(seekUpdate, 1000);
+    currentSong.addEventListener('ended', nextSong);
+    // random_bg_color();
+
+}
+
+function resetValues() {
+    currTime.textContent = "00:00";
+    totalDuration.textContent = "00:00";
+    seekSlider.value = 0;
+}
+
+// Load the first song in the songlist
+//loadSong(songIndex);
+
+function playpauseSong(currentBtn) {
+    if (!isPlaying) playSong(currentBtn);
+    else pauseSong(currentBtn);
+}
+
+function playSong(currentBtn) {
+    if (currentBtn) {
+        loadSong(currentBtn.value);
+        currentBtn.innerHTML = '<i class="fa fa-pause-circle fa-2x"></i>';
+    }
+    currentSong.play(); 
+    isPlaying = true;
+    playpauseBtn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+
+function pauseSong(currentBtn) {
+    if(currentBtn){
+        currentBtn.innerHTML = '<i class="fa fa-play-circle fa-2x"></i>';
+    }
+    currentSong.pause();
+    isPlaying = false;
+    playpauseBtn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+}
+
+function nextSong() {
+    //it is not needed to write tho
+    if (repeatMode) {
+        currentSongIndex = currentSongIndex;
+    }
+    if (suffleMode) {
+        currentSongIndex = Math.floor(Math.random() * playListBackUp.length)
+    }
+    if (defaultMode) {
+        if (currentSongIndex < playListBackUp.length - 1) {
+            currentSongIndex += 1;
+        }
+        else {
+            currentSongIndex = 0;
+        }
+    }
+    debugger
+    loadSong(currentSongIndex);
+    playSong();
+}
+
+function prevSong() { 
+    if (repeatMode) {
+        currentSongIndex = currentSongIndex;
+    }
+    if (suffleMode) {
+        currentSongIndex = Math.floor(Math.random() * playListBackUp.length)
+    }
+    if (defaultMode) {
+        if (currentSongIndex > 0) {
+            currentSongIndex -= 1;
+        }
+        else {
+            currentSongIndex = playListBackUp.length - 1;
+        }
+    }
+
+    loadSong(currentSongIndex);
+    playSong();
+}
+
+function seekTo() {
+    let seekto = currentSong.duration * (seekSlider.value / 100);
+    currentSong.currentTime = seekto;
+}
+
+function setVolume() {
+    currentSong.volume = volumeSlider.value / 100;
+}
+
+function seekUpdate() {
+    let seekPosition = 0;
+
+    if (!isNaN(currentSong.duration)) {
+        seekPosition = currentSong.currentTime * (100 / currentSong.duration);
+
+        seekSlider.value = seekPosition;
+
+        let currentMinutes = Math.floor(currentSong.currentTime / 60);
+        let currentSeconds = Math.floor(currentSong.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(currentSong.duration / 60);
+        let durationSeconds = Math.floor(currentSong.duration - durationMinutes * 60);
+
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+        currTime.textContent = currentMinutes + ":" + currentSeconds;
+        totalDuration.textContent = durationMinutes + ":" + durationSeconds;
+    }
+}
+
+
+function setPlayMode() {
+    setModeCount++;
+    switch (setModeCount) {
+        case 1: {
+            defaultMode = false;
+            repeatMode = true;
+            suffleMode = false;
+            playMode.innerHTML = '<i class="fa fa-repeat fa-2x"></i>'
+            break;
+        }
+        case 2: {
+            defaultMode = false;
+            repeatMode = false;
+            suffleMode = true;
+            playMode.innerHTML = '<i class="fa fa-random fa-2x"></i>';
+            break;
+        }
+        default:
+            {
+                playMode.innerHTML = '<i class="fas fa-circle fa-2x"></i>';
+                defaultMode = true;
+                repeatMode = false;
+                suffleMode = false;
+                setModeCount = 0;
+            }
+    }
+}
+
+
 
 
 
